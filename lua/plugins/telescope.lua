7270bbc18479c95cea26c4819a25b175feef6980
Danylo Korsun <dakorsun@gmail.com>
2023-03-13 21:26:59 +0200

    quick fix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -160,8 +160,8 @@ vim.keymap.set('n', '<leader>ps', function()
 end)
 
 
-telescope.load_extension("fzf")
-telescope.load_extension("live_grep_args")
-telescope.load_extension("ui-select")
+-- telescope.load_extension("fzf")
+-- telescope.load_extension("live_grep_args")
+-- telescope.load_extension("ui-select")
 
 return M
