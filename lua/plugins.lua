76559a852f9cdc259b8c8d55364011ba1bf1ad62
Danylo Korsun <dakorsun@gmail.com>
2023-03-13 20:11:31 +0200

    quick fix

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -38,9 +38,9 @@ require("packer").startup(function()
   -- use("L3MON4D3/LuaSnip")
   -- use("neovim/nvim-lspconfig") -- Collection of configurations for built-in LSP client
   -- use({
-    "hrsh7th/nvim-cmp",
-    requires = { { "onsails/lspkind-nvim" } },
-  })
+  --  "hrsh7th/nvim-cmp",
+  --  requires = { { "onsails/lspkind-nvim" } },
+  --})
   -- use("hrsh7th/cmp-nvim-lsp")
   -- use("hrsh7th/cmp-buffer")
   -- use("hrsh7th/cmp-cmdline")
